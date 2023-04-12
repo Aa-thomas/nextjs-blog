72776efed727c0f78763e4a40e4bf3251a155cfd
Aa-thomas <aarthomas01@gmail.com>
2023-04-11 20:00:58 -0400

    implement getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.scss';
 
-export default function Home() {
+export const getStaticProps = async () => {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+};
+
+export default function Home({ allPostsData }) {
+	console.log;
 	return (
 		<Layout home>
 			<Head>
 				<title>{siteTitle}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
 				<p>
 					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea,
 					odit. Optio doloribus nisi autem alias laudantium. Minus at
