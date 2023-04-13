7983a433c7c4703b7c16b7ebe9924d87e63850c4
Aa-thomas <aarthomas01@gmail.com>
2023-04-13 18:55:33 -0400

    render markdown using remark lib

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,20 +1,19 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
+export const getStaticProps = async ({ params }) => {
+	const postData = await getPostData(params.id);
+	return {
+		props: {
+			postData,
+		},
+	};
+};
 export const getStaticPaths = async () => {
 	const paths = getAllPostIds();
 	return {
 		paths,
 		fallback: false,
-	};
-};
-
-export const getStaticProps = ({ params }) => {
-	const postData = getPostData(params.id);
-	return {
-		props: {
-			postData,
-		},
 	};
 };
 
@@ -26,6 +25,7 @@ export const Post = ({ postData }) => {
 			{postData.id}
 			<br />
 			{postData.date}
+			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
 		</Layout>
 	);
 };
